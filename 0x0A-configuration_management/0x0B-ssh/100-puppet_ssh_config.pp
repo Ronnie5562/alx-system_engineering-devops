8a9df1123ffff5a1eca60b9bd4686e783450cbc2
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-23 21:11:49 +0200

    Task 3

---
+++ 0x0A-configuration_management/0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# we’d like you to set up your client SSH configuration file so that you can connect to a server without typing a password.
+include stdlib
+
+file_line { 'Disable password auth':
+  ensure  => present,
+  path    => '~/.ssh/config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '~/.ssh/config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
