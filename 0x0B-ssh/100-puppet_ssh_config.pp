77e39effb5ad8181d1e6f92f6171e6c3c0bb7410
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-23 21:31:11 +0200

    Task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,14 +4,14 @@ include stdlib
 
 file_line { 'Turn off passwd auth':
   ensure  => present,
-  path    => '~/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Declare identity file':
   ensure  => present,
-  path    => '~/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => '     IdentityFile ~/.ssh/school',
   replace => true,
 }
