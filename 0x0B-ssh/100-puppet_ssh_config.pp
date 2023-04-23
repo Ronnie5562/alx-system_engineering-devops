fe1ff8f04545a476f1d2fc7a76f00714d6fea9da
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-23 21:28:17 +0200

    Readme added

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,8 @@
 # we’d like you to set up your client SSH configuration file so that you can connect to a server without typing a password.
+
 include stdlib
 
-file_line { 'Disable password auth':
+file_line { 'Turn off passwd auth':
   ensure  => present,
   path    => '~/.ssh/config',
   line    => '    PasswordAuthentication no',
